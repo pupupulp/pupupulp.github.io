09f7209b87f5bb52fad2833e94a52ab1bc811bae
Eagan Martin <mece.martinece@gmail.com>
2019-05-10 07:42:50 +0800

    Added layouts

---
+++ js/main.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+
+  'use strict';
+  
+  // smooth scrolling
+  $(document).on("scroll", onScroll);
+
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function() {
+      $(this).removeClass('active');
+      if ($(window).width() < 768) {
+        $('.nav-menu').slideUp();
+      }
+    });
+
+    $(this).addClass('active');
+
+    var target = this.hash,
+        menu = target;
+
+    target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': target.offset().top - 80
+    }, 500, 'swing', function() {
+      window.location.hash = target.selector;
+      $(document).on("scroll", onScroll);
+    });
+  });
+
+  function onScroll(event) {
+    if ($('.home').length) {
+      var scrollPos = $(document).scrollTop();
+      $('nav ul li a').each(function() {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+      });
+    }
+  }
+
+  // navigation toggle
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 200 ) {
+      $("#main-nav, #main-nav-subpage").slideDown(700);
+      $("#main-nav-subpage").removeClass('subpage-nav');
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $("#main-nav").slideUp(700);
+      $("#main-nav-subpage").hide();
+      $("#main-nav-subpage").addClass('subpage-nav');
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  // responsive menu
+  $('.responsive').on('click', function(e) {
+    $('.nav-menu').slideToggle();
+  });
+
+  // typedjs
+  $(function() {
+    $(".typed").typed({
+      strings: [
+        "I'm Eagan Charles Martin.", 
+        "I'm an application developer.", 
+        "I love photography.",
+        "I enjoy playing games.",
+      ],
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
+
+  // glitch
+  $(function() {
+    $(".glitch-img").mgGlitch({
+      destroy: false,
+      glitch: true,
+      scale: false,
+      blend: true,
+      blendModeType: 'hue',
+      glitch1TimeMin: 200, 
+      glitch1TimeMax: 400,
+      glitch2TimeMin: 100,
+      glitch2TimeMax: 100,
+      zIndexStart: 0, 
+    });
+  });
+
+  // counter
+  $('.counter').counterUp({
+		delay: 15,
+		time: 2000
+  });
+  
+  // back to top
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+});
